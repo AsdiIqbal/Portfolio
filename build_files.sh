861dfee6e732291cc3b82125fbad28f35872a24c
Asad Iqbal <asadr.iqbal@gmail.com>
2024-04-17 14:54:10 +0500

    updated

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build the project
 echo "Building the project...ABC"
-python3.9 -m pip3 install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 echo "Completely built"
 
 echo "Make Migration..."
