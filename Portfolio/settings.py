12c0567fbf85625a9f988b569d1628d90d9fc658
Asad Iqbal <asadr.iqbal@gmail.com>
2024-04-17 12:26:13 +0500

    edited

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os 
 
 from pathlib import Path
 
@@ -23,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-468&2!67jbq!_m0izv1-jwl56-c2l&=kb4fv8glmq7u3&-w*i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [".vercel.app"]
+DEBUG = False
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'YHdwfThxbEmrTnSTNrIsTDmlbfrajLyt',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '41250',
     }
 }
 
@@ -129,3 +133,6 @@ BASE_DIR,'static',
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
