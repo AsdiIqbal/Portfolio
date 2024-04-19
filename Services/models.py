fb12b439854896338d8a92b1db4676274be2755f
Asad Iqbal <asadr.iqbal@gmail.com>
2024-04-19 14:51:19 +0500

    Updated 19042024

--- Services/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Features(models.Model):
-
-    Name = models.CharField(max_length=200)
-    Age =  models.IntegerField()
-    Status = models.CharField(max_length=200)
